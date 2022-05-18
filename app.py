c1596d821d4e25660eee7026effb5d193193d330
Edelina Alieva <edelina.alieva@gmail.com>
2022-05-17 20:09:05 -0400

    Page_not_found added and a layout page

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def configuration():
+    """ Guest configuration of the race """
+
     return "<p>Hello, World!</p>"
+
+@app.route("/<username>")
+def user_configuration():
+    """ Users configuration of the race """
+
+    return "TODO"
+
