e3f72675ba573b44d475e77e9895ce5ead989e68
Edelina Alieva <edelina.alieva@gmail.com>
2022-06-05 02:52:05 -0400

    Typer done, unlimited done

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, redirect, render_template, session, url_for, f
 from werkzeug.security import check_password_hash, generate_password_hash
 from tempfile import mkdtemp
 from random import randrange
-import time
 import sqlite3
 from random import shuffle
 import numpy as np
@@ -11,11 +10,6 @@ app = Flask(__name__)
 
 # Configurations
 app.secret_key = 'b1b92f2cd885248e85ba872c3399628d7062bf375d58fa0042f95afaf62a318e'
-
-# Open a file for the directionary
-f = open("words.txt")
-words = f.readlines()
-f.close()
 
 @app.route("/", methods=["GET", "POST"])
 def configuration():
@@ -39,12 +33,6 @@ def register():
 @app.route("/unlimited", methods=["POST"])
 def time():
     """ Configure with dictionary """
-    shuffle(words)
-    newarr = np.array_split(words, 5000)
-    text = ""
-
-    for word in newarr[0]:
-        text = text + word
 
     minutes = request.form.get("minutes")
     seconds = request.form.get("seconds")
@@ -63,7 +51,8 @@ def time():
         if not 'user_id' in session:
             cur.execute('INSERT INTO requests (minutes, seconds, user, configuration) VALUES (?, ?, ?, ?)', (minutes, seconds, "guest", "time limit"))
             con.commit()
-            return render_template("keyboard.html", player="GUEST", word="love", text=text)
+            flash("Type to start.")
+            return render_template("keyboard.html", player="GUEST", seconds=seconds, minutes=minutes)
         cur.execute('INSERT INTO requests (minutes, seconds, user, configuration) VALUES (?, ?, ?, ?)', (minutes, seconds, session['user_id'], "time limit"))
         con.commit()
         return TODO
--- static/myscripts.js
+++ static/myscripts.js
@@ -1,13 +1,122 @@
-const {readFileSync, promises: fsPromises} = require('fs');
-const word = document.querySelector("#word");
-const text = document.querySelector("#text");
-const timer = document.querySelector("#time");
+import {words} from './words.js'
+const text = document.querySelector("#textbox");
+const userInput = document.querySelector("#user");
+const changingSec = document.querySelector('#sec');
+const changingMin = document.querySelector('#min');
+const minutes = parseInt(document.querySelector("#minutes").innerHTML);
+const seconds = parseInt(document.querySelector("#seconds").innerHTML);
+let min = 0, sec = 0;
+let counter = 0;
+let upward = 0;
+var hasStarted = false;
+let typed_entries = 0;
 
-// Word list
-try {
-    const contents = readFileSync('words.txt', 'utf-8');
-    const words = contents.split(/\r?\n/);
-    console.log(words);
-} catch (err) {
-    console.log(err);
+
+// Started words
+add_words_print(7);
+
+// Choose random words
+function random_word() {
+    return words[Math.floor(Math.random() * (466549 - 0))];
 }
+
+// Update time
+function update_time() {
+    if (parseInt(changingSec.innerHTML) >= 59) {
+        min++;
+        sec = 0;
+        changingMin.innerHTML = min;
+        changingSec.innerHTML = sec;
+    }
+    else {
+        sec++;
+        if (sec < 10) {
+            changingSec.innerHTML = "0" + sec;
+        }
+        else {
+            changingSec.innerHTML = sec;
+        }
+    }
+}
+
+// Print the words
+function add_words_print(times) {
+    let number = 0;
+    do {
+        text.innerHTML = text.innerHTML + '<span id="word' + counter + '">' + random_word() + "</span> ";
+        counter++;
+        number++;
+    } while (number != times)
+}
+
+// Check if the word is right
+function check() {
+    let word = document.querySelector("#word" + upward).innerHTML;
+    let input = userInput.value;
+    changeColor(word, input);
+    if (input.localeCompare(word) == 0) {
+        upward_word();
+    }
+}
+
+// Change the word color
+function changeColor(word, input) {
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] != word[i]) {
+            document.querySelector("#word" + upward).style.color = "red";
+            return;
+        }
+    }
+
+    document.querySelector("#word" + upward).style.color = "green";
+}
+
+// Change the upward word
+function upward_word() {
+    typed_entries += parseInt(document.querySelector("#word" + upward).innerHTML.length);
+    document.querySelector("#word" + upward).remove();
+    upward++;
+    userInput.value = "";
+    add_words_print(1);
+}
+
+// Calculate the time needed
+function time_need() {
+    return minutes * 60 + seconds;
+}
+
+// Set the timer
+function timer_set() {
+    var needed = time_need();
+    var timesRun = 0;
+    var interval = setInterval(function() {
+        timesRun++;
+        userInput.oninput = function onEvent(event) {
+            speed(timesRun);
+            check();
+        }
+        if(timesRun === needed){
+            clearInterval(interval);
+            userInput.oninput = function onEvent(event) {
+                event.stopPropagation();
+                alert('Timeout');
+            }
+        }
+        update_time();
+    }, 1000);
+    hasStarted = false;
+}
+
+// Count speed
+function speed(time) {
+    document.querySelector("#speed").innerHTML = Math.round((typed_entries/5)/(time/60));
+}
+
+// Program running
+userInput.onkeypress = function () {
+    if(!hasStarted) {
+        timer_set();
+        hasStarted = true;
+    }
+}
+
